aa5843646746f65e8e74f955857643d6b3e06972
nikhilsciex <nikhil.lale1@sciex.com>
2025-04-10 20:34:30 +0530

    Implement standalone search for header.

---
+++ scripts/header-search/headerSearchController.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { buildStandaloneSearchBox } from 'https://static.cloud.coveo.com/headless/v3/headless.esm.js';
+import { searchEngine }  from './headerSearchEngine.js';
+
+export const standaloneSearchBoxController = buildStandaloneSearchBox(searchEngine, {
+  options: {
+    redirectionUrl: '/search-results',
+    numberOfSuggestions: 5,
+    highlightOptions: {
+      notMatchDelimiters: {
+        open: '<strong>',
+        close: '</strong>',
+      },
+      correctionDelimiters: {
+        open: '<i>',
+        close: '</i>',
+      },
+    },
+  },
+});
