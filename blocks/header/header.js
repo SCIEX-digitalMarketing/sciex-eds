225f592c400afd8a33ec315036f5a3d926a71108
subrahmanyam <subrahmanyam.y@gspann.com>
2025-06-11 16:32:13 +0530

    Logout button showing issue fix

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1450,14 +1450,14 @@ export default async function decorate(block) {
 
   // Conditionally shwoing the login/logout links
   const userData = await getUserDetails();
-  const eloquaData = {
-    status: userData.loggedIn,
-    email: userData.email,
-    key: userData.userKey,
-  };
-  sessionStorage.setItem('loggedin-status', userData.loggedIn);
-  sessionStorage.setItem('eloquaData', JSON.stringify(eloquaData));
   if (userData && userData.loggedIn) {
+    const eloquaData = {
+      status: userData.loggedIn,
+      email: userData.email,
+      key: userData.userKey,
+    };
+    sessionStorage.setItem('loggedin-status', userData.loggedIn);
+    sessionStorage.setItem('eloquaData', JSON.stringify(eloquaData));
     document.getElementById('view-profile').style.display = '';
     document.getElementById('logout').style.display = '';
     document.getElementById('register').style.display = 'none';
