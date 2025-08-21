3bfdd309ed0eeec113112a4b618829b7cce96476
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-08-21 15:56:17 +0530

    Sciex-text block issue fix

--- blocks/sciex-text/sciex-text.js
+++ blocks/sciex-text/sciex-text.js
@@ -1,19 +1,37 @@
 import { } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  let blockId = block.children[0].textContent;
-  if (blockId && blockId.trim() !== '') {
-    blockId = blockId.trim();
-  } else {
-    blockId = 'sciex-text';
+  console.log("length>>>"+block.children.length);
+  let blockId = 'sciex-text';
+  let alignment = 'text-left';
+  let content;
+   if (block.children.length === 3) {
+    blockId = block.children[0]?.textContent?.trim() || 'sciex-text';
+    alignment = block.children[1]?.textContent?.trim() || 'text-left';
+    content = block.children[2];
+  } else if (block.children.length === 2) {
+    const maybeAlignment = block.children[0]?.textContent?.trim();
+    if (maybeAlignment === 'text-left' || maybeAlignment === 'text-right' || maybeAlignment === 'text-center') {
+      alignment = maybeAlignment;
+    }else{
+       blockId = block.children[0]?.textContent?.trim() || 'sciex-text';
+    }
+    content = block.children[1];
+  } else if (block.children.length === 1) {
+    const singleChild = block.children[0];
+    const text = singleChild?.textContent?.trim();
+    if (singleChild.querySelector('h3')) {
+      content = singleChild;
+    } else if (['text-left', 'text-right', 'text-center'].includes(text)) {
+      alignment = text;
+    } else if (
+      singleChild.querySelectorAll('*').length === 1 &&
+      singleChild.querySelector('p')
+    ) {
+      blockId = text || 'sciex-text';
+    }
   }
-  let alignment = block.children[1].textContent;
-  if (alignment && alignment.trim() !== '') {
-    alignment = alignment.trim();
-  } else {
-    alignment = 'text-left';
-  }
-  const content = block.children[2];
+
   block.id = `${blockId}-content`;
   block.className = 'sciex-text';
   block.parentElement.classList.add('tabs-container-wrapper');
--- blocks/tabs-container/tabs-container.js
+++ blocks/tabs-container/tabs-container.js
@@ -7,6 +7,7 @@ export default async function decorate(block) {
   blockDiv.classList.add('technotes-main');
 
   [...block.children].forEach((row, rowIndex) => {
+    console.log("row>>>"+row.outerHTML);
     if (rowIndex === 0) {
       block.id = `${row.textContent.trim().replace(/\s+/g, '-')}-content`;
     } else {
