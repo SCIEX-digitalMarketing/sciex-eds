2715b4a5d31cf880f69e784efdd162a934911dbf
sushanth-sciex <sushanth.baddam@sciex.com>
2025-09-04 21:21:07 +0530

    Wrong end point usage

--- scripts/header-search/headerSearchEngine.js
+++ scripts/header-search/headerSearchEngine.js
@@ -3,7 +3,7 @@ import { buildSearchEngine } from 'https://static.cloud.coveo.com/headless/v3/he
 
 export const searchEngine = buildSearchEngine({
   configuration: {
-    organizationId: 'danahernonproduction1892f3fhz',
+    organizationId: 'danaherproductionrfl96bkr',
     accessToken: 'xx2136ae2c-554b-41a7-8afd-1ad8c7ffcb07',
     search: {
       searchHub: 'SCIEXMainSearch',
