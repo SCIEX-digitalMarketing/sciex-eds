83a5774104da01ef8bf8d17f309057693710787c
Anandhan <srinivasu.kondi@sciex.com>
2026-02-26 14:15:46 +0530

    fix

--- scripts/blocks-controllers/controller.js
+++ scripts/blocks-controllers/controller.js
@@ -1,6 +1,7 @@
 export const getPartnersData = async () => {
   try {
-    const endpoint = `${location.origin}/bin/sciex/partners`;
+    // const endpoint = `${location.origin}/bin/sciex/partners`;
+    const endpoint = `https://author-p93412-e854706.adobeaemcloud.com/bin/sciex/partners`;
     const response = await fetch(endpoint);
 
     if (!response.ok) {
