e1cdf04e1c22961119bc1aceff697e656ebe8583
Anandhan <srinivasu.kondi@sciex.com>
2026-05-05 12:37:14 +0530

    icon

--- blocks/course-catalog-detail/course-catalog-detail.js
+++ blocks/course-catalog-detail/course-catalog-detail.js
@@ -106,8 +106,8 @@ export default async function decorate(block) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 22"
-          width="20"
-          height="20"
+          width="30"
+          height="30"
           fill="none"
           stroke="#000"
           stroke-width="1.5"
